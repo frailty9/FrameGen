bbd5255a691002cfa6ab19b6743c145ee97c0386
花殇 <ydl9263@qq.com>
2025-07-02 10:42:44 +0800

    feat: 默认使用Hikari数据源实现

---
+++ framegen-core/src/main/java/org/framegen/config/JdbcConfig.java
@@ -0,0 +1,102 @@
+package org.framegen.config;
+
+import lombok.Data;
+
+import java.util.Objects;
+import java.util.Properties;
+
+@Data
+public final class JdbcConfig {
+    private final String url;
+    private final String driverClassName;
+    private final String username;
+    private final String password;
+    private final Properties additionalProps;
+
+    private JdbcConfig(Builder builder) {
+        this.url = builder.url;
+        this.driverClassName = builder.driverClassName;
+        this.username = builder.username;
+        this.password = builder.password;
+        this.additionalProps = builder.additionalProps;
+    }
+
+    public Properties toProperties() {
+        Properties props = new Properties();
+        props.setProperty("jdbcUrl", this.url);
+        props.setProperty("driverClassName", this.driverClassName);
+        props.setProperty("username", this.username);
+        props.setProperty("password", this.password);
+        props.putAll(this.additionalProps);
+        return props;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static final class Builder {
+        private String url;
+        private String driverClassName;
+        private String username;
+        private String password;
+        private final Properties additionalProps = new Properties();
+
+        public static Builder builder() {
+            return new Builder();
+        }
+
+        public Builder url(String url) {
+            this.url = Objects.requireNonNull(url, "url cannot be null");
+            return this;
+        }
+
+        public Builder driverClassName(String driverClassName) {
+            this.driverClassName = Objects.requireNonNull(driverClassName, "driverClassName cannot be null");
+            return this;
+        }
+
+        public Builder username(String username) {
+            this.username = Objects.requireNonNull(username, "username cannot be null");
+            return this;
+        }
+
+        public Builder password(String password) {
+            this.password = Objects.requireNonNull(password, "password cannot be null");
+            return this;
+        }
+
+        public Builder config(String key, String value) {
+            this.additionalProps.setProperty(key, value);
+            return this;
+        }
+
+        public Builder connectionTimeout(long milliseconds) {
+            return this.config("connectionTimeout", String.valueOf(milliseconds));
+        }
+
+        public Builder idleTimeout(long milliseconds) {
+            return this.config("idleTimeout", String.valueOf(milliseconds));
+        }
+
+        public Builder maximumPoolSize(int size) {
+            return this.config("maximumPoolSize", String.valueOf(size));
+        }
+
+        public JdbcConfig build() {
+            if (this.url == null) {
+                throw new IllegalStateException("url is required");
+            }
+            if (this.driverClassName == null) {
+                throw new IllegalStateException("driverClassName is required");
+            }
+            if (this.username == null) {
+                throw new IllegalStateException("username is required");
+            }
+            if (this.password == null) {
+                throw new IllegalStateException("password is required");
+            }
+            return new JdbcConfig(this);
+        }
+    }
+}
---
+++ framegen-core/src/main/java/org/framegen/core/db/impl/HikariDataSourceGetter.java
@@ -0,0 +1,33 @@
+package org.framegen.core.db.impl;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.framegen.core.db.DataSourceFactory;
+import org.framegen.config.JdbcConfig;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+public class HikariDataSourceGetter implements DataSourceFactory {
+
+    private final Properties configProps;
+
+    /**
+     * 如果您的项目原本就使用HikariCP作为数据库连接池,
+     * 那么您能够直接传入HikariCP的配置信息,
+     * @param configProps HikariCP的配置信息
+     */
+    public HikariDataSourceGetter(Properties configProps) {
+        this.configProps = configProps;
+    }
+
+    public HikariDataSourceGetter(JdbcConfig jdbcConfig) {
+        this(jdbcConfig.toProperties());
+    }
+
+    @Override
+    public DataSource getDataSource() {
+        HikariConfig config = new HikariConfig(this.configProps);
+        return new HikariDataSource(config);
+    }
+}
