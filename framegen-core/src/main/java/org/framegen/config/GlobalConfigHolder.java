d9ffd52454e0a3d9a657f205ed55024f4babec6a
花殇 <ydl9263@qq.com>
2025-07-07 16:20:14 +0800

    feat: 全局配置类

---
+++ framegen-core/src/main/java/org/framegen/config/GlobalConfigHolder.java
@@ -0,0 +1,11 @@
+package org.framegen.config;
+
+public class GlobalConfigHolder {
+
+    public static boolean enableKotlin = false;
+
+    public static boolean enableLombok = false;
+
+    public static boolean enableMultiThread = false;
+
+}
