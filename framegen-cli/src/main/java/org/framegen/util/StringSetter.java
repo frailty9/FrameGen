91f2fb053e0c4db7bd2edfff0ebe6c6f61305b10
花殇 <ydl9263@qq.com>
2025-09-25 15:24:57 +0800

    docs: javadoc中的尖括号使用转义字符

--- framegen-cli/src/main/java/org/framegen/util/StringSetter.java
+++ framegen-cli/src/main/java/org/framegen/util/StringSetter.java
@@ -2,7 +2,7 @@ package org.framegen.util;
 
 /**
  * 一个方法类型
- * 用于解决直接使用 Function<String, Void> 时无法传入传统setter方法的问题
+ * 用于解决直接使用 <b>Function&lt;String, Void&gt;</b> 时无法传入传统setter方法的问题
  */
 @FunctionalInterface
 public interface StringSetter {
