2f07f5142df3b09e76fa36b988178dc9546e575e
花殇 <ydl9263@qq.com>
2025-09-26 10:46:05 +0800

    refactor: 使用lombok替代手写Builder模式

--- framegen-core/src/main/java/org/framegen/core/generator/props/GeneratorProps.java
+++ framegen-core/src/main/java/org/framegen/core/generator/props/GeneratorProps.java
@@ -1,11 +1,15 @@
 package org.framegen.core.generator.props;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Collection;
 
 @Data
+@Builder(builderClassName = "Builder")
+@NoArgsConstructor
 @AllArgsConstructor
 public class GeneratorProps<T> {
     private String packagePath;
@@ -14,55 +18,4 @@ public class GeneratorProps<T> {
     private String classComment;
     private String className;
     private T data;
-
-    public static <T> Builder<T> builder() {
-        return new Builder<T>();
-    }
-
-    public static class Builder<T> {
-        private String packagePath;
-        private Collection<String> imports;
-        private Collection<String> annotations;
-        private String classComment;
-        private String className;
-        private T data;
-
-        Builder() {
-        }
-
-        public Builder<T> packagePath(String packagePath) {
-            this.packagePath = packagePath;
-            return this;
-        }
-
-        public Builder<T> imports(Collection<String> imports) {
-            this.imports = imports;
-            return this;
-        }
-
-        public Builder<T> annotations(Collection<String> annotations) {
-            this.annotations = annotations;
-            return this;
-        }
-
-        public Builder<T> classComment(String classComment) {
-            this.classComment = classComment;
-            return this;
-        }
-
-        public Builder<T> className(String className) {
-            this.className = className;
-            return this;
-        }
-
-        public Builder<T> data(T data) {
-            this.data = data;
-            return this;
-        }
-
-        public GeneratorProps<T> build() {
-            return new GeneratorProps<T>(packagePath, imports, annotations, classComment, className, data);
-        }
-        
-    }
 }
