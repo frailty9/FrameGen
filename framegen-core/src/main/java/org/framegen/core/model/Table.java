3f46245f74e9def30a0bc379b96fa8189ba538f1
花殇 <ydl9263@qq.com>
2025-10-13 10:29:36 +0800

    fix: 修复同一个类型多次出现会多次import的问题

--- framegen-core/src/main/java/org/framegen/core/model/Table.java
+++ framegen-core/src/main/java/org/framegen/core/model/Table.java
@@ -36,7 +36,7 @@ public class Table {
 
     public Column getPrimaryColumn() {
         return columns.stream()
-               .filter(column -> column.getColumnKey().contains("PRI"))
+                .filter(column -> column.getColumnKey().contains("PRI"))
                 .collect(Collectors.toList()).get(0);
     }
 
@@ -53,21 +53,27 @@ public class Table {
         typeImports = new ArrayList<>();
         columns.forEach(column -> {
             String codeType = column.getDataType();
+            String importType = null;
             switch (codeType) {
                 case "BigDecimal":
-                    typeImports.add("java.math.BigDecimal");
+                    importType = "java.math.BigDecimal";
                     break;
                 case "LocalDate":
-                    typeImports.add("java.time.LocalDate");
+                    importType = "java.time.LocalDate";
                     break;
                 case "LocalTime":
-                    typeImports.add("java.time.LocalTime");
+                    importType = "java.time.LocalTime";
                     break;
                 case "LocalDateTime":
-                    typeImports.add("java.time.LocalDateTime");
+                    importType = "java.time.LocalDateTime";
                     break;
                 default:
                     break;
+            }
+            String finalImportType = importType;
+            if (null != importType && typeImports.stream().noneMatch(
+                    type -> type.equals(finalImportType))) {
+                typeImports.add(importType);
             }
         });
     }
